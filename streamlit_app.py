9500002674a06e2757f90dee63009107410be78c
JKean22 <113358675+JKean22@users.noreply.github.com>
2022-10-19 16:56:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,8 @@ if streamlit.button('Add a Fruit to the List'):
  back_from_function = insert_row_snowflake(add_my_fruit)
  streamlot.text(back_from_function)                                                         
 
-
+streamlit.stop()
+                                                          
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "jackfruit")
